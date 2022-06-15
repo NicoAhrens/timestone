74d97ed7f4a8566487fa16dd19512977d01c5647
NicoAhrens <nico_ahrens89@web.de>
2022-06-15 16:33:25 +0200

    remove variable 'timestamp' from log_date_time()

--- timestone.py
+++ timestone.py
@@ -13,35 +13,58 @@ from connection import single_command
 
 # TODO: Create a function which gets the time es an input and dependend on the
 # input of the user, connects with the postgreSQL Databse over the psycopg2
-# library. Creates a query for the user to look up specific times and dates to 
+# library. Creates a query for the user to look up specific times and dates to
 # update, correct or calculate the worked time on that date.
 
 
 def start_screen():
+    """
+   Printing the Start Screen/Logo
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
     lines =\
         [
             "",
             ("⬗ TIMESTONE ⬖\n").center(gts().columns),
             ("Keeping track of your time!").center(gts().columns), "\n"
-            #("Press Enter").center(gts().columns)
         ]
     for line in lines:
         print(line)
 
 
 def main_menu():
+    """
+    Printing out the menu
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
     options =\
         [
             "Press [1]: Start Main Programm",
             "Press [2]: Calculations",
             "Press [X]: Quit the Programm"
         ]
-    # clearConsole()
     for option in options:
         print(option)
 
 
 def logging_menu():
+    """
+    Printing out the menu
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
+
     terminal_width = gts().columns
     options =\
         [
@@ -60,6 +83,14 @@ def logging_menu():
 
 
 def calculations_menu():
+    """
+    Printing out the menu for calculations
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
     terminal_width = gts().columns
     options =\
         [
@@ -76,38 +107,31 @@ def calculations_menu():
 
 
 def clearConsole():
+    """
+   Clear the displayed console
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
     command = 'clear'
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
 
-# TODO: Mabye without package keyboard. Have to look into it how it really works
-# def keypress():
-#     while True:
-#         try:
-#             if keyboard.is_pressed('i'):
-#                 return 'i'
-#             elif keyboard.is_pressed('o'):
-#                 return 'o'
-#             elif keyboard.is_pressed('b'):
-#                 return 'b'
-#             elif keyboard.is_pressed('l'):
-#                 return 'l'
-#             elif keyboard.is_pressed('q'):
-#                 return 'q'
-#             elif keyboard.is_pressed('x'):
-#                 return 'x'
-#             elif keyboard.is_pressed('1'):
-#                 return '1'
-#             elif keyboard.is_pressed('2'):
-#                 return '2'
-#             elif keyboard.is_pressed('3'):
-#                 return '3'
-#         except KeyboardError:
-#             raise Exception('Wrong input')  # if user pressed a key other than the given key the loop will break
-
 
 def log_event(date, time, status_id):
+    """
+    Printing out the menu
+    -----
+    Parameters:
+        date []
+        time []
+        status_id []
+    Returns:
+        None
+    """
     command = (
                 f"""
                 INSERT INTO timekeeping(date, time, employee_id, status_id)
@@ -117,7 +141,16 @@ def log_event(date, time, status_id):
     single_command(command)
 
 
-def log_date_time(timestamp):
+def log_date_time():
+    """
+    Gets the date and time from a timestamp
+    -----
+    Parameters:
+        None
+    Returns:
+        date [str]: Stripped date from the timestamp
+        time [str]: Stripped time from the timestamp
+    """
     timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     timestamp_split = timestamp.split()
     date = timestamp_split[0]
@@ -125,6 +158,14 @@ def log_date_time(timestamp):
     return date, time
 
 def timestamp():
+    """
+    Workflow
+    -----
+    Parameters:
+        None
+    Returns:
+        None
+    """
     # ======= Creates necessary tables in the database =======
     command = None
     timestamp = None
@@ -133,7 +174,7 @@ def timestamp():
     esc_timekeeping = False
     esc_calc = False
     clearConsole()
-    
+
     while not esc_main:
         start_screen()
         main_menu()
@@ -159,22 +200,23 @@ def timestamp():
                 user_input_log = input("> ")
 
                 if user_input_log == 'i':
-                    date, time = log_date_time(timestamp)
+                    date, time = log_date_time()
                     status_id = '1'
                     print('Logged in.')
+                    print(f"Stamped in Date: {date} and Time: {time}")
                     log_event(date, time, status_id)
                 elif user_input_log == 'o':
-                    date, time = log_date_time(timestamp)
+                    date, time = log_date_time()
                     status_id = '2'
                     print('Logged out.')
                     log_event(date, time, status_id)
                 elif user_input_log == 'b':
-                    date, time = log_date_time(timestamp)
+                    date, time = log_date_time()
                     status_id = '3'
                     print('Logged into break. Enjoy your meal! :)')
                     log_event(date, time, status_id)
                 elif user_input_log == 'l':
-                    date, time = log_date_time(timestamp)
+                    date, time = log_date_time()
                     status_id = '4'
                     print('Logged out of break. Hope you enjoyed your meal. :)')
                     log_event(date, time, status_id)
